6c0b03bb0f96b70e9cfda07f943d698012dd58a8
Ilyosbek Najmiddinov <ilyosbeknajmiddinov055@gmail.com>
2022-09-10 05:18:28 +0500

    example of operator and operator overloading

--- HelloWorld/src/Main.cpp
+++ HelloWorld/src/Main.cpp
@@ -2,38 +2,60 @@
 #include <string>
 #include "Log.h"
 
-class Printable
+struct Vector2
 {
-public:
-	virtual std::string GetClassName() = 0;
+	float x, y;
+
+	Vector2(float x, float y)
+		: x(x), y(y) {}
+
+	Vector2 Add(const Vector2& other) const
+	{
+		return Vector2(x + other.x, y + other.y);
+	}
+
+	Vector2 operator+(const Vector2& other) const
+	{
+		return Add(other);
+	}
+
+	Vector2 Multiply(const Vector2& other) const
+	{
+		return Vector2(x * other.x, y * other.y);
+	}
+
+	Vector2 operator*(const Vector2& other) const
+	{
+		return Multiply(other);
+	}
+
+	bool operator==(const Vector2& other) const
+	{
+		return x == other.x && y == other.y;
+	}
+
+	bool operator!=(const Vector2& other) const
+	{
+		return !operator==(other);
+	}
 };
 
-class Entity : public Printable
+std::ostream& operator<<(std::ostream& stream, const Vector2& other)
 {
-public:
-	std::string GetClassName() override { return "Entity"; }
-};
-
-class Player : public Entity
-{
-public:
-	std::string GetClassName() override { return "Player"; }
-};
-
-void Print(Printable* obj)
-{
-	std::cout << obj->GetClassName() << std::endl;
-}
-
-void PrintString(const std::string& string)
-{
-	std::cout << string << std::endl;
+	stream << other.x << ", " << other.y;
+	return stream;
 }
 
 int main()
 {
-	std::string name = std::string("Ilyosbek") + " hello!";
-	PrintString(name);
+	Vector2 position(4.0f, 4.0f);
+	Vector2 speed(0.5f, 1.5f);
+	Vector2 powerup(1.1f, 1.1f);
+
+	Vector2 result1 = position.Add(speed.Multiply(powerup));
+	Vector2 result2 = position + speed * powerup;
+
+	std::cout << result2 << std::endl;
 
 	std::cin.get();
 }
